738a89ef8ecceb8498a3942e800db5305ba2431a
tim <timothy.tran835@gmail.com>
2018-11-29 15:37:27 -0800

    changed controller to have correct functionality

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -1,12 +1,11 @@
-const products = require('../../database/models/products');
+const { ProductPostgres } = require('../../database/models/products');
 const utils = require('../../utils/utils');
 
 const controller = {
   get: (req, res) => {
     var recordId = Math.floor(Math.random() * 1000);
     // let recordId = 50;
-    products
-      .findById(recordId)
+    ProductPostgres.findById(recordId)
       .then(response => {
         console.log(response.dataValues);
         res.status(200).send(response.dataValues);
