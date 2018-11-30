514d376cb4c23b49127c1824bf11348797742aa0
tim <timothy.tran835@gmail.com>
2018-11-29 20:09:32 -0800

    changed to ip only

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -18,7 +18,7 @@ export default class App extends Component {
       });
     axios.defaults.baseURL =
       // 'http://localhost:' + process.env.PORT;
-      'http://54.218.66.243:3001';
+      '54.218.66.243:3001';
   }
 
   componentDidMount() {
